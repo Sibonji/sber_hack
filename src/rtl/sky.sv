13a0b8b963c7f13f67017cdf8821e7d9570ecd94
Siburai <sibgatullin.ba@phystech.edu>
2024-11-09 11:41:53 +0300

    Added red square as car

--- src/rtl/sky.sv
+++ src/rtl/sky.sv
@@ -30,8 +30,9 @@ module sky #(
   //--------- Regime                             --------//
     output wire  [1:0]    regime_status
 );
+    logic [1:0] demo_regime_status;
     logic state;
-    logic [0:0] frame_cnt;
+    logic [0:0] frames_cntr;
     logic [4:0] ch_dir;
     logic end_of_frame;
     logic h_end, v_end;
@@ -63,6 +64,96 @@ module sky #(
     assign par_b = {1'b1, random_par[12:7]};
     assign par_b_pulled = 11'd100;
     assign mult_res = (base_y * base_y) >> 8;
+
+    // car gen 50*90
+    parameter CAR_V = 70;
+    parameter CAR_H = 40;
+    parameter CAR_START_V = V_PIXELS - 150;
+    parameter CAR_START_H = H_PIXELS / 2 - CAR_H / 2;
+
+    reg       [1:0]   regime_store  ;         // Two demonstration regimes
+    wire              change_regime ;
+    //----------------------- Counters                     --------------------------//
+    parameter         FRAMES_PER_ACTION = 2;  // Action delay
+    //----------------------- Accelerometr                 --------------------------//
+    parameter     ACCEL_X_CORR = 8'd3;        // Accelerometer x correction
+    parameter     ACCEL_Y_CORR = 8'd1;        // Accelerometer y correction
+    wire   [7:0]  accel_data_x_corr  ;        // Accelerometer x corrected data
+    wire   [7:0]  accel_data_y_corr  ;        // Accelerometer y corrected data
+
+    logic [10:0]   object_h_coord     ;         // Object Point(P) horizontal coodrinate
+    logic [10:0]   object_v_coord     ;         // Object Point(P) vertical coordinate
+    logic [10:0]   object_h_speed     ;         // Horizontal Object movement speed
+
+    //------------------------- Regime control               ----------------------------//
+    always @ ( posedge pixel_clk ) begin //Right now there are 2 regimes
+        if ( !rst_n ) begin
+            regime_store <= 2'b11;
+        end
+        else if (change_regime && (regime_store == 2'b10)) begin
+            regime_store <= 2'b11;
+        end
+        else if ( change_regime ) begin
+            regime_store <= regime_store - 1'b1;
+        end
+    end
+    assign change_regime      = button_c    ;
+    assign demo_regime_status = regime_store;
+
+    //------------------------- Accelerometr at the end of frame-------------------------//
+    always @ ( posedge pixel_clk ) begin
+        if ( !rst_n ) begin
+            accel_x_end_of_frame <= 8'h0000000;
+            accel_y_end_of_frame <= 8'h0000000;
+        end
+        else if ( end_of_frame && (frames_cntr == 0) ) begin
+            accel_x_end_of_frame <= accel_data_x_corr;
+            accel_y_end_of_frame <= accel_data_y_corr;
+        end
+    end
+    // Accelerometr corrections
+    assign accel_data_x_corr = accel_data_x + ACCEL_X_CORR;
+    assign accel_data_y_corr = accel_data_y + ACCEL_Y_CORR;
+
+    //------------------------- Object movement in 2 regimes  ----------------------------//
+    assign object_h_speed = 11'd1;
+    always @ ( posedge pixel_clk ) begin
+        if ( !rst_n ) begin // Put object in the center
+            object_h_coord <= H_PIXELS / 2 - CAR_H / 2;
+            object_v_coord <= V_PIXELS - 150;
+        end
+        else if ( end_of_frame && (frames_cntr == 0) ) begin
+            if (regime_store == 2'b11) begin  // Buttons regime
+                if ( button_l ) begin           // Moving left
+                    if ( object_h_coord < object_h_speed)
+                        object_h_coord <= 0;
+                    else
+                        object_h_coord <= object_h_coord - object_h_speed;
+                end
+                else if ( button_r ) begin
+                    if ( object_h_coord + object_h_speed + CAR_H >= 10'd799 )
+                        object_h_coord <= 10'd799 - CAR_H;
+                    else
+                        object_h_coord <= object_h_coord + object_h_speed;
+                end
+            end
+            else if (regime_store == 2'b10) begin  // Accelerometer regime
+                if      ( !accel_data_y_corr[7] && ( accel_data_y_corr != 8'h00 )) begin
+                    if ( object_h_coord < object_h_speed)
+                        object_h_coord <= 0;
+                    else
+                        object_h_coord <= object_h_coord - object_h_speed;
+                end
+                else if ( accel_data_y_corr[7] && ( accel_data_y_corr != 8'h00 ) ) begin
+                    if ( object_h_coord + object_h_speed + CAR_H >= 10'd799 )
+                        object_h_coord <= 10'd799 - CAR_H;
+                    else
+                        object_h_coord <= object_h_coord + object_h_speed;
+                end
+            end
+        end
+    end
+
     
     random random_inst(
         .clk    ( pixel_clk  ),
@@ -82,7 +173,7 @@ module sky #(
             always_ff @( posedge pixel_clk ) begin
                 if ( !rst_n )
                     road[i] <= H_PIXELS / 2 - ROAD_WIDTH / 2;
-                else if ( &frame_cnt && end_of_frame )
+                else if ( &frames_cntr && end_of_frame )
                     road[i] <= road[i - 1];
             end
         end
@@ -92,9 +183,10 @@ module sky #(
         if ( !rst_n ) begin
             road[0] <= H_PIXELS / 2 - ROAD_WIDTH / 2;
         end
-        else if ( &frame_cnt && end_of_frame) begin
+        else if ( &frames_cntr && end_of_frame) begin
             $display(road[0], base_x, mult_res[10:0], base_x + mult_res[10:0] - par_b_pulled);
             $display("Part: ", par_part, ", direction: ", direction, ", Road type: ", road_type[1], road_type[0], ", v len: ", cur_type_len);
+            $display("Left: ", button_l, ", right: ", button_r);
             if ( !road_type[1] )
                 road[0] <= direction ? (road[0] + 1) : (road[0] - 1);
             else if ( road_type[1] ) begin
@@ -119,7 +211,7 @@ module sky #(
             base_y <= 16'd160;
             par_part <= '0;
         end
-        else if ( &frame_cnt && end_of_frame && road_type[1] ) begin
+        else if ( &frames_cntr && end_of_frame && road_type[1] ) begin
             if ( base_y == 16'd0 ) begin
                 par_part <= '1;
                 base_y <= base_y + 1;
@@ -138,7 +230,7 @@ module sky #(
         else if ( cur_type_len == 9'd0 )
             cur_type_len <= random_num[8:0];
             // cur_type_len <= 200;
-        else if ( &frame_cnt && end_of_frame )
+        else if ( &frames_cntr && end_of_frame )
             cur_type_len <= cur_type_len - 1;
     end
     
@@ -180,29 +272,43 @@ module sky #(
     
     always_ff @( posedge pixel_clk ) begin
         if ( !rst_n )
-            frame_cnt <= '0;
+            frames_cntr <= '0;
         else if ( end_of_frame )
-            frame_cnt <= frame_cnt + 1;
+            frames_cntr <= frames_cntr + 1;
     end
-
-    logic [10:0]curr_road;
-    assign curr_road = road[v_coord];
     
     always_ff @( posedge pixel_clk ) begin
         if ( !rst_n ) begin
-            red <= '0;
-            green <= '0;
-            blue <= 4'd8;
-        end
-        else if ( (h_coord >= road[v_coord]) && (h_coord <= road[v_coord] + ROAD_WIDTH)) begin
             red <= '1;
             green <= '1;
             blue <= '1;
         end
-        else begin
+        else if ( (v_coord >= CAR_START_V) && (v_coord <= CAR_START_V + CAR_V) ) begin
+            if ( (h_coord >= object_h_coord) && (h_coord <= object_h_coord + CAR_H) ) begin
+                red <= 4'd8;
+                green <= '0;
+                blue <= '0;
+            end
+            else if ( (h_coord >= road[v_coord]) && (h_coord <= road[v_coord] + ROAD_WIDTH)) begin
+                red <= '0;
+                green <= '0;
+                blue <= 4'd8;
+            end
+            else begin
+                red <= '1;
+                green <= '1;
+                blue <= '1;
+            end
+        end
+        else if ( (h_coord >= road[v_coord]) && (h_coord <= road[v_coord] + ROAD_WIDTH)) begin
             red <= '0;
             green <= '0;
             blue <= 4'd8;
+        end
+        else begin
+            red <= '1;
+            green <= '1;
+            blue <= '1;
         end
     end
     
